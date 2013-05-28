ae77dedd20df1fd3dea868c64d7d598945337bf0
Shawn Neal <sneal@sneal.net>
2013-05-28 15:28:47 -0700

    Fixed missing node variable. Cookbook now works with version

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 version = node['notepadplusplus']['version']
 checksum = node['notepadplusplus'][version]['checksum']
-url = "#{['notepadplusplus']['base_url']}/#{version}/npp.#{version}.Installer.exe"
+url = "#{node['notepadplusplus']['base_url']}/#{version}/npp.#{version}.Installer.exe"
 
 windows_package node['notepadplusplus']['package_name'] do
   source url
