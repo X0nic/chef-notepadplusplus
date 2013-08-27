bf7bee707b2565900f2285d401b8d7c26e96fc86
Ian Gantt <igantt@daptiv.com>
2013-08-27 11:00:12 -0700

    Add the notepad++ command to the environment path when attribute set to true

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,6 @@ default['notepadplusplus']['package_name'] = 'Notepad++'
 
 default['notepadplusplus']['6.3.3']['checksum'] = 
   '007282ae45aef944c195874db02002c364c9851d04d92485d1cfc4c9c77e1e85'
+
+# Set to true to add the notepad++ command to the environment path
+default['notepadplusplus']['add_to_path'] = true
