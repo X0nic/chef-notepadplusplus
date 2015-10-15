8979aed4c9e95db4c4197df1a6dd57c935996cdf
Judith Hartmann <judith.hartmann@student.hpi.uni-potsdam.de>
2015-10-15 11:58:14 +0100

    Update download url since old one was 404

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['notepadplusplus']['url']          = "http://download.tuxfamily.org/notepadplus/5.9.8/npp.5.9.8.Installer.exe"
+default['notepadplusplus']['url']          = "https://notepad-plus-plus.org/repository/6.x/6.8.3/npp.6.8.3.Installer.exe"
 default['notepadplusplus']['package_name'] = "Notepad++"
